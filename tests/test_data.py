51a55309446227393ab73f4ac137c96c8f358d1d
Davide Mecugni <davidemecugni@gmail.com>
2026-01-11 07:39:54 +0100

    Added testing

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,7 +3,16 @@ from torch.utils.data import Dataset
 from proj.data import MyDataset
 
 
-def test_my_dataset():
-    """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+class TestClass:
+    def test_my_dataset(self):
+        """Test the MyDataset class."""
+        dataset = MyDataset("data/raw")
+        assert isinstance(dataset, Dataset)
+
+    def test_my_dataset_methods(self):
+        """Test the methods of MyDataset class."""
+        dataset = MyDataset("data/raw")
+        assert hasattr(dataset, "preprocess")
+        assert hasattr(dataset, "spec_to_tensors")
+        assert hasattr(dataset, "__len__")
+        assert hasattr(dataset, "__getitem__")
